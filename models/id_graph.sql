e663b66ce4a43970798e75c8f989e25cbe597368
Matheus Santos <49241368+matsantos33@users.noreply.github.com>
2023-11-07 15:04:10 -0300

    Update id_graph.sql

--- models/id_graph.sql
+++ models/id_graph.sql
@@ -2,25 +2,44 @@
 
 SELECT
     rudder_id,
-    node,
-    {{ dbt.listagg('DISTINCT node_label', "', '") }} AS labels,
-    MAX(node_timestamp) AS latest_timestamp
-FROM (
-    SELECT
-        rudder_id,
-        node_a AS node,
-        node_a_label AS node_label,
-        node_timestamp
-    FROM {{ ref('edges') }}
-    UNION
-    SELECT
-        rudder_id,
-        node_b AS node,
-        node_b_label AS node_label,
-        node_timestamp
-    FROM {{ ref('edges') }}
-) AS c
+    edge as node,
+    {{ dbt_utils.listagg('DISTINCT edge_label', '","') }} AS label,
+    MAX(edge_timestamp) AS latest_timestamp
+    
+FROM 
+    (
+        SELECT
+            rudder_id,
+            edge_a AS edge,
+            edge_a_label AS edge_label,
+            edge_timestamp
+        FROM {{ ref('edges') }}
+
+        UNION DISTINCT
+
+        SELECT
+            rudder_id,
+            edge_b AS edge,
+            edge_b_label AS edge_label,
+            edge_timestamp
+        FROM {{ ref('edges') }}
+    ) AS c
+
 GROUP BY
     rudder_id,
-    node
+    edge
 ORDER BY rudder_id
+
+/*
+
+    Esse código é usado para criar uma tabela chamada "id_graph" que agrupa as arestas (edges) e seus rótulos (labels) 
+    por identificadores (rudder_id) e arestas, e mantém a data e hora mais recente das arestas (edge_timestamp).
+
+    Ele faz isso unindo as arestas do lado A e B e, em seguida, agrupando-as pelo identificador (rudder_id) e arestas. 
+    Ele também usa a função listagg para concatenar os rótulos (edge_label) das arestas em uma única coluna de texto separada por vírgulas.
+
+    A tabela "id_graph" armazena essas informações agrupadas para facilitar a visualização e análise das relações 
+    entre os identificadores e as arestas, e as suas labels.
+
+
+*/
