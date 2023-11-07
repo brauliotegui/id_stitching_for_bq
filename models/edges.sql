b6f51090b64ece58f3da49c8f8403aaf09bf7d44
Matheus Santos <49241368+matsantos33@users.noreply.github.com>
2023-11-07 15:04:20 -0300

    Update edges.sql

--- models/edges.sql
+++ models/edges.sql
@@ -1,3 +1,5 @@
+--Combines the results of those select statement to create a table containing edges (IDs) the first time it is run, and matches edges on subsequent runs.
+
 {{ config(materialized='incremental', unique_key='original_rudder_id') }}
 
 {% if not is_incremental() %}
@@ -5,119 +7,135 @@
     {% set sql_statements = dbt_utils.get_column_values(table=ref('queries'), column='sql_to_run') %}
 
     SELECT
-        ROW_NUMBER() OVER (ORDER BY 1 DESC) AS rudder_id,
-        ROW_NUMBER() OVER (ORDER BY 1 DESC) AS original_rudder_id,
-        node_a,
-        node_a_label,
-        node_b,
-        node_b_label,
-        {{ dbt.current_timestamp() }} AS node_timestamp
+        ROW_NUMBER() OVER (ORDER BY 1 DESC) AS  rudder_id,
+        ROW_NUMBER() OVER (ORDER BY 1 DESC) AS  original_rudder_id,
+        edge_a,
+        edge_a_label,
+        edge_b,
+        edge_b_label,
+        {{ dbt_utils.current_timestamp() }} AS  edge_timestamp
     FROM (
-        {{ ' UNION '.join(sql_statements) }}
-    ) AS s
-    {% if var('ids-to-exclude', undefined) %}
+        {{ ' UNION DISTINCT '.join(sql_statements) }}
+    ) AS  s
     WHERE
-        NOT LOWER(node_a) IN {{ var('ids-to-exclude') }}
-        AND NOT LOWER(node_b) IN {{ var('ids-to-exclude') }}
-    {% endif %}
+        NOT LOWER(edge_a) in {{ var('ids-to-exclude') }}
+        AND NOT LOWER(edge_b) in {{ var('ids-to-exclude') }}
 
 {% else %}
 
     WITH
-    cte_min_node_1 AS (
+    cte_min_edge_1 AS  (
         SELECT
-            node,
-            MIN(rudder_id) AS first_row_id
+            edge,
+            MIN(rudder_id) AS  first_row_id
         FROM (
             SELECT
                 rudder_id,
-                LOWER(node_a) AS node
-            FROM {{ this }}
-            UNION
+                LOWER(edge_a) AS  edge
+            FROM {{ this  }}
+            UNION DISTINCT
             SELECT
                 rudder_id,
-                LOWER(node_b) AS node
-            FROM {{ this }}
+                LOWER(edge_b) AS  edge
+            FROM {{ this  }}
         ) AS c
-        GROUP BY node
+        GROUP BY edge
     ),
 
-    cte_min_node_2 AS (
+    cte_min_edge_2 AS  (
         SELECT
-            node,
-            MIN(rudder_id) AS first_row_id
+            edge,
+            MIN(rudder_id) AS  first_row_id
         FROM (
             SELECT
-                LEAST(a.first_row_id, b.first_row_id) AS rudder_id,
-                LOWER(o.node_a) AS node
-            FROM {{ this }} AS o
-            LEFT OUTER JOIN cte_min_node_1 AS a
-                ON LOWER(o.node_a) = a.node
-            LEFT OUTER JOIN cte_min_node_1 AS b
-                ON LOWER(o.node_b) = b.node
-            UNION
+                LEAST(a.first_row_id, b.first_row_id) AS  rudder_id,
+                LEAST(o.edge_a) AS  edge
+            FROM {{ this  }} AS  o
+            LEFT OUTER JOIN cte_min_edge_1 AS  a
+                ON LOWER(o.edge_a) = a.edge
+            LEFT OUTER JOIN cte_min_edge_1 AS  b
+                ON LOWER(o.edge_b) = b.edge
+            UNION DISTINCT
             SELECT
-                LEAST(a.first_row_id, b.first_row_id) AS rudder_id,
-                LOWER(o.node_b) AS node
-            FROM {{ this }} AS o
-            LEFT OUTER JOIN cte_min_node_1 AS a
-                ON LOWER(o.node_a) = a.node
-            LEFT OUTER JOIN cte_min_node_1 AS b
-                ON LOWER(o.node_b) = b.node
-
-        ) AS g
-        GROUP BY node
+                LEAST(a.first_row_id, b.first_row_id) AS  rudder_id,
+                LOWER(o.edge_b) AS  edge
+            FROM {{ this  }} AS  o
+            LEFT OUTER JOIN cte_min_edge_1 AS  a
+                ON LOWER(o.edge_a) = a.edge
+            LEFT OUTER JOIN cte_min_edge_1 AS  b
+                ON LOWER(o.edge_b) = b.edge
+        ) AS  g
+        GROUP BY edge
     ),
 
-    cte_min_node_3 AS (
+    cte_min_edge_3 AS  (
         SELECT
-            node,
-            MIN(rudder_id) AS first_row_id
+            edge,
+            MIN(rudder_id) AS  first_row_id
         FROM (
             SELECT
-                LEAST(a.first_row_id, b.first_row_id) AS rudder_id,
-                LOWER(o.node_a) AS node
-            FROM {{ this }} AS o
-            LEFT OUTER JOIN cte_min_node_2 AS a
-                ON LOWER(o.node_a) = a.node
-            LEFT OUTER JOIN cte_min_node_2 AS b
-                ON LOWER(o.node_b) = b.node
-            UNION
+                LEAST(a.first_row_id, b.first_row_id) AS  rudder_id,
+                LOWER(o.edge_a) AS  edge
+            FROM {{ this  }} AS  o
+            LEFT OUTER JOIN cte_min_edge_2 AS  a
+                ON LOWER(o.edge_a) = a.edge
+            LEFT OUTER JOIN cte_min_edge_2 AS  b
+                ON LOWER(o.edge_b) = b.edge
+            UNION DISTINCT
             SELECT
-                LEAST(a.first_row_id, b.first_row_id) AS rudder_id,
-                LOWER(o.node_b) AS node
-            FROM {{ this }} AS o
-            LEFT OUTER JOIN cte_min_node_2 AS a
-                ON LOWER(o.node_a) = a.node
-            LEFT OUTER JOIN cte_min_node_2 AS b
-                ON LOWER(o.node_b) = b.node
+                LEAST(a.first_row_id, b.first_row_id) AS  rudder_id,
+                LOWER(o.edge_b) AS  edge
+            FROM {{ this  }} AS  o
+            LEFT OUTER JOIN cte_min_edge_2 AS  a
+                ON LOWER(o.edge_a) = a.edge
+            LEFT OUTER JOIN cte_min_edge_2 AS  b
+                ON LOWER(o.edge_b) = b.edge
 
-        ) AS h
-        GROUP BY node
+        ) AS  h
+        GROUP BY edge
     ),
 
-    cte_new_id AS (
+    cte_new_id AS  (
         SELECT
             o.original_rudder_id,
-            LEAST(a.first_row_id, b.first_row_id) AS new_rudder_id
-        FROM {{ this }} AS o
-        LEFT OUTER JOIN cte_min_node_3 AS a
-            ON LOWER(o.node_a) = a.node
-        LEFT OUTER JOIN cte_min_node_3 AS b
-            ON LOWER(o.node_b) = b.node
+            LEAST(a.first_row_id, b.first_row_id) AS  new_rudder_id
+        FROM {{ this  }} AS  o
+        LEFT OUTER JOIN cte_min_edge_3 AS  a
+            ON LOWER(o.edge_a) = a.edge
+        LEFT OUTER JOIN cte_min_edge_3 AS  b
+            ON LOWER(o.edge_b) = b.edge
     )
 
     SELECT
-        cte_new_id.new_rudder_id AS rudder_id,
+        cte_new_id.new_rudder_id AS  rudder_id,
         e.original_rudder_id,
-        e.node_a,
-        e.node_a_label,
-        e.node_b,
-        e.node_b_label,
-        {{ dbt.current_timestamp() }} AS node_timestamp
-    FROM {{ this }} AS e
+        e.edge_a,
+        e.edge_a_label,
+        e.edge_b,
+        e.edge_b_label,
+        {{ dbt_utils.current_timestamp() }} AS  edge_timestamp
+    FROM {{ this  }} AS  e
     INNER JOIN cte_new_id
         ON e.original_rudder_id = cte_new_id.original_rudder_id
     WHERE e.rudder_id != cte_new_id.new_rudder_id
 
 {% endif %}
+
+/*
+    A tabela "edges.sql" tem as seguintes colunas:
+
+    rudder_id: número da linha gerado pela tabela edges
+    original_rudder_id: número da linha gerado pela tabela queries
+    edge_a: valor do identificador único encontrado na tabela de origem
+    edge_a_label: label do identificador único encontrado na tabela de origem
+    edge_b: valor do identificador único mapeado
+    edge_b_label: label do identificador único mapeado
+    edge_timestamp: data e hora de quando a aresta foi criada
+
+    Na primeira execução, ele seleciona os valores distintos das colunas "edge_a" e "edge_b" das tabelas selecionadas, 
+    renomeando-os como "edge_a", "edge_a_label", "edge_b" e "edge_b_label" respectivamente, 
+    e adiciona as colunas "rudder_id", "original_rudder_id" e "edge_timestamp"
+
+    Na execução incremental, ele irá comparar as novas arestas com as arestas existentes 
+    e irá manter a aresta com o menor "rudder_id" como a aresta válida.
+*/
