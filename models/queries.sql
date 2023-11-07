b7421d36d01b18dd9523e6bf49e8e6bdf885cc77
Matheus Santos <49241368+matsantos33@users.noreply.github.com>
2023-11-07 15:03:50 -0300

    Update queries.sql

--- models/queries.sql
+++ models/queries.sql
@@ -2,28 +2,29 @@ WITH columns AS (
     SELECT
         column_name AS cn,
         table_catalog || '.' || table_schema || '.' || table_name AS tn
-    FROM "{{ var('source-database', target.database) }}".{{ source('information_schema', 'columns').include(database=false) }}
+    FROM `grupo-coutinho.rudderstack.INFORMATION_SCHEMA.COLUMNS`
     WHERE
         LOWER(column_name) IN {{ var('id-columns') }}
-        {% if var('schemas-to-include', undefined) %}
-        AND LOWER(table_schema) IN {{ var('schemas-to-include') }}
-        {% endif %}
-        {% if var('schemas-to-exclude', undefined) %}
-        AND NOT LOWER(table_schema) IN {{ var('schemas-to-exclude') }}
-        {% endif %}
-        {% if var('tables-to-include', undefined) %}
-        AND LOWER(table_name) IN {{ var('tables-to-include') }}
-        {% endif %}
-        {% if var('tables-to-exclude', undefined) %}
-        AND NOT LOWER(table_name) IN {{ var('tables-to-exclude') }}
-        {% endif %}
         AND NOT LOWER(table_name) LIKE 'snapshot_%'
         AND NOT LOWER(table_name) LIKE 'sync_data_%'
         AND NOT LOWER(table_name) LIKE 'failed_records_%'
 )
 
-SELECT 'SELECT DISTINCT ' || a.cn || '::TEXT AS node_a, ''' || a.cn || ''' AS node_a_label, ' || b.cn || '::TEXT AS node_b, ''' || b.cn || ''' AS node_b_label FROM ' || a.tn || ' WHERE COALESCE(' || a.cn || '::TEXT, '''') != '''' AND COALESCE(' || b.cn || '::TEXT, '''') != ''''' AS sql_to_run
+SELECT
+    'SELECT DISTINCT '
+    || a.cn
+    || ' AS  edge_a, '
+    || a.cn
+    || ' AS  edge_a_label, '
+    || b.cn
+    || ' AS  edge_b, '
+    || b.cn
+    || ' AS  edge_b_label FROM '
+    || a.tn
+    || ' WHERE ('
+    || a.cn
+    || ') IS NOT NULL AND ('
+    || b.cn
+    || ') IS NOT NULL ' AS sql_to_run
 FROM columns AS a
-INNER JOIN columns AS b
-    ON a.tn = b.tn
-        AND a.cn > b.cn
+INNER JOIN columns AS b ON a.tn = b.tn AND a.cn > b.cn
